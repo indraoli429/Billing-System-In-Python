36d0d6ebb4601b8a4401b534a386d2ac9d3d1723
Indra Bahadur Oli <indraoli429@gmail.com>
2021-02-17 12:37:19 +0545

    pep 8 mantain

--- read.py
+++ read.py
@@ -1,16 +1,17 @@
-def read_file(): #Function is defined with name : 'read_file'
-    file=open("products.txt","r") #open stock file (products.txt) in read mode. 
-    lines=file.readlines() 
-    L=[] # assign empty listwith name 'L'
+
+
+def read_file():    # Function is defined with name : 'read_file'
+    file = open("products.txt", "r")    # open stock file (products.txt) in read mode.
+    lines = file.readlines()
+    L = []  # assign empty list with name 'L'
     for line in lines:
-        L.append(line.replace("\n","").split(","))
+        L.append(line.replace("\n", "").split(","))
     file.close()
     print("Following products are avilable in our Store")
     print("--------------------------------------------")
     print("PRODUCT\t\tPRICE\t\tQUANTITY")
     print("--------------------------------------------")
     for i in range(len(L)):
-        print(L[i][0],"\t\t",L[i][1],"\t\t",L[i][2]) # prints the availabile product, price and quantity
+        print(L[i][0], "\t\t", L[i][1], "\t\t", L[i][2])    # prints the available product, price and quantity
     print("--------------------------------------------")
     return L
-    
